b45e1e4204ff018d74c9fcd5b14ab98397a4d46e
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-03-26 10:47:47 +0100

    initial commit

---
+++ src/main/java/com/discaCoders/apiWarehouse424/ApiWarehouse424Application.java
@@ -0,0 +1,13 @@
+package com.discaCoders.apiWarehouse424;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWarehouse424Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWarehouse424Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/discaCoders/apiWarehouse424/ApiWarehouse424ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discaCoders.apiWarehouse424;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWarehouse424ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
