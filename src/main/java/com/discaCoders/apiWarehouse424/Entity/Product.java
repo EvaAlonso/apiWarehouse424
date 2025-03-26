7c922396ddde8140240d813d1595dce906d72ae9
lltosso <luis.tosso@gmail.com>
2025-03-26 12:01:43 +0100

    product entity added

--- src/main/java/com/discaCoders/apiWarehouse424/Entity/Product.java
+++ src/main/java/com/discaCoders/apiWarehouse424/Entity/Product.java
@@ -1,4 +1,73 @@
 package com.discaCoders.apiWarehouse424.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private  String name;
+    @Column(name = "quantity")
+    private int quantity;
+    @Column(name = "price")
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
 }
